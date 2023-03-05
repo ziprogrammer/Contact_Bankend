110935674254b3d0dff67b1930dd3b040ddd2a9f
ziprogrammer <www.zigale2016@gmail.com>
2023-03-06 02:35:02 +0300

    Connaction string is worked

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -8,6 +8,7 @@ const connectDb = async () => {
       connect.connection.host,
       connect.connection.name
     );
+    mongoose.set('strictQuery', true);
   } catch (err) {
     console.log(err);
     process.exit(1);
